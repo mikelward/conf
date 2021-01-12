b3fa3346f66e0b6c25c6db53bf49f8890178b269
Mikel Ward <mbw@google.com>
2021-01-11 16:21:29 -0800

    Reformat the keys section.

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -108,19 +108,9 @@ for i in groups:
     keys.extend(
         [
             # mod + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(toggle=True),
-                desc="Switch to group {}".format(i.name),
-            ),
+            Key([mod], i.name, lazy.group[i.name].toscreen(toggle=True), desc="Switch to group {}".format(i.name)),
             # mod + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
+            Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True), desc="Switch to & move focused window to group {}".format(i.name)),
         ]
     )
 
